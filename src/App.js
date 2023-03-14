5199dcee5866d900491861c82e1f99a5b805e0ce
sakibsarker <sakib15-3824@diu.edu.bd>
2023-03-15 01:51:07 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css'
 import { Provider } from 'react-redux'
 import CakeContainer from './components/CakeContainer'
 import store from './redux/store'
+import HooksCakeContainer from './components/HooksCakeContainer'
 
 function App() {
   return (
    <Provider store={store}>
     <div>
+      <HooksCakeContainer/>
       <CakeContainer/>
       </div> 
 
